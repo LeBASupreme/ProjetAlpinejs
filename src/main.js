449788b2d73e6826af7eca1c28ab0d643ac3b40b
AnessFOUKA <foukaaness@gmail.com>
2026-01-12 22:31:39 +0100

    first frontend version

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import Alpine from "alpinejs";
 window.Alpine=Alpine;
+document.addEventListener("alpine:init",()=>{
+    Alpine.store("data",{
+        projects:["pj1","pj2"],
+        username:"John doey"
+    })
+});
 Alpine.start();
