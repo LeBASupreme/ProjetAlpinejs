990e9b469379a7d272ad189ba6cbf13a7551b034
AnessFOUKA <foukaaness@gmail.com>
2026-01-13 17:39:44 +0100

    forms are now working

--- src/main.js
+++ src/main.js
@@ -3,7 +3,32 @@ window.Alpine=Alpine;
 document.addEventListener("alpine:init",()=>{
     Alpine.store("data",{
         projects:["pj1","pj2"],
-        username:"John doey"
+        tasks:[],
+        username:"John doey",
+        addTaskFormFields:{
+            "Nom":"",
+            "Description":""
+        },
+        addProjectFormFields:{
+            "Nom":"",
+        },
+        showAddTaskForm:false,
+        showAddProjectsForm:false,
+        addTask:function(){
+            //todo pending finished
+            this.tasks.push({Nom:this.addTaskFormFields.Nom,Description:this.addTaskFormFields.Description,Statut:"Todo"});
+            for(let i in this.addTaskFormFields){
+                this.addTaskFormFields[i]="";
+            }
+            this.showAddTaskForm=!this.showAddTaskForm;
+        },
+        addProject:function(){
+            this.projects.push(this.addProjectFormFields.Nom);
+            for(let i in this.addProjectFormFields){
+                this.addProjectFormFields[i]="";
+            }
+            this.showAddProjectsForm=!this.showAddProjectsForm;
+        }
     })
 });
 Alpine.start();
