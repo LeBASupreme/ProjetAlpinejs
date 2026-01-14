0c8309e261ecd8fbc2f06ecb10e3dc792f1e5269
AnessFOUKA <foukaaness@gmail.com>
2026-01-14 18:01:56 +0100

    14/01/2026 18h01 work milestone

--- src/main.js
+++ src/main.js
@@ -9,17 +9,25 @@ document.addEventListener("alpine:init",()=>{
         addTaskFormFields:{
             "Nom":"",
             "Description":"",
-            "Date début (format xxxx-xx-xx)":"",
-            "Date fin (format xxxx-xx-xx)":""
+            "Date début":"",
+            "Date fin":""
         },
         addProjectFormFields:{
             "Nom":"",
         },
         showAddTaskForm:false,
         showAddProjectsForm:false,
+        showUpdateForm:false,
+        toYMD:function (date) {
+            console.log(date)
+            const y = date.getFullYear()
+            const m = String(date.getMonth() + 1).padStart(2, '0')
+            const d = String(date.getDate()).padStart(2, '0')
+            return `${y}-${m}-${d}`
+        },
         addTask:function(){
             //todo pending finished
-            this.tasks.push({TaskId:Date.now(),Nom:this.addTaskFormFields.Nom,Description:this.addTaskFormFields.Description,Statut:"Todo",dateDeb:new Date(this.addTaskFormFields["Date début (format xxxx-xx-xx)"]),dateFin:new Date(this.addTaskFormFields["Date fin (format xxxx-xx-xx)"])});
+            this.tasks.push({TaskId:Date.now(),ProjectId:this.pickedProjectId,Nom:this.addTaskFormFields.Nom,Description:this.addTaskFormFields.Description,Statut:"Todo","Date début":new Date(this.addTaskFormFields["Date début"]),"Date fin":new Date(this.addTaskFormFields["Date fin"])});
             for(let i in this.addTaskFormFields){
                 this.addTaskFormFields[i]="";
             }
@@ -32,7 +40,26 @@ document.addEventListener("alpine:init",()=>{
                 this.addProjectFormFields[i]="";
             }
             this.showAddProjectsForm=!this.showAddProjectsForm;
+        },
+        modifyTask:function(TaskId){
+            for(let i of this.tasks){
+                if(i.TaskId==TaskId){
+                    for(let j in i){
+                        if(Object.keys(this.addTaskFormFields).includes(j)){
+                            i[j]=this.addTaskFormFields[j];
+                        }
+                    }
+                }
+            }
+            this.addTaskFormFields={
+                "Nom":"",
+                "Description":"",
+                "Date début":"",
+                "Date fin":""
+            };
+            this.showUpdateForm=!this.showUpdateForm;
         }
     })
 });
+//relier chaque task à un projet
 Alpine.start();
